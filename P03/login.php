cce06cf93569c9fbe26b64f57954ab32776bbd26
HifniKhakim <112689567+HifniKhakim@users.noreply.github.com>
2022-12-05 12:58:52 +0700

    Add files via upload

---
+++ P03/login.php
@@ -0,0 +1,25 @@
+<?php
+echo "<pre>";
+var_dump($_GET);
+echo "<pre>";
+var_dump($_POST);
+echo "<pre>";
+
+?>
+<ol>
+    <li>ali</li>
+    <li></li>
+    <li></li>
+    <li></li>
+</0l>
+
+
+Formulir
+<form method="post" enctype="multipart/form-data">>
+username :
+<input name=nim><br>
+password:
+<input type=file name=profil><br>
+<input type=password name=password><br>
+<button>submit</button>
+</form>
